0cdea906b042e2c8b899e57dea54473ac29cbff2
MD Shamiul Haque <mdhaque@clarkson.edu>
2020-02-06 20:45:55 -0500

    Create histoondate.py
    Histoon Date from a bunch of files

---
+++ histoondate.py
@@ -0,0 +1,30 @@
+import sys,time,os,json,datetime,operator
+
+#SELECT * FROM cddata WHERE Date > 2019-01-01 and Date < 2019-01-20 
+dateCount = {}
+for root,dirs, files in os.walk('data'):
+    #print(root,dirs,files)
+    path = root.split(os.sep)
+    for fn in files:
+        fp = root+os.sep+fn
+        print(fp)
+        f = open(fp, 'r')
+        for line in f:
+            data = json.loads(line)
+            payload = data['payload']
+            payload = json.loads(payload)
+#            print(payload['received'])
+            k = payload['received'].split('T')[0]
+            #print (k)
+            if k in dateCount.keys():
+                dateCount[k]+=1
+            else:
+                dateCount[k] = 1
+
+# 0 == key , 1 == value
+sorted_dc = sorted(dateCount.items(),key=operator.itemgetter(1),reverse=True)
+
+print(sorted_dc)
+
+
+import sys,time,os,json,datetime,operator
